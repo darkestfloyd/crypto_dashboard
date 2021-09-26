6a8897da506cb763e46d1bd80fd28bc1c085c880
Nischal Mahaveer Chand <nischal.mahaveerchand@berghealth.com>
2021-09-26 12:52:25 -0400

    Update utils file

--- utils.R
+++ utils.R
@@ -37,7 +37,7 @@ binance_sync <- function(...) {
 
 create_blotter_portfolio <- function(p_name, trades, start_date = '2021-02-01') {
     currency("USD")
-    coins <- trades$yfin_coin
+    coins <- unique(trades$yfin_coin)
     # coins <- c("BTC-USD", "ETH-USD", "ADA-USD", "XLM-USD", "BNB-USD", "LINK-USD", 
                # "ALGO-USD", "DOGE-USD", "VET-USD", "EOS-USD", "MATIC-USD", "VTHO-USD", 
                # "MANA-USD")
@@ -45,7 +45,7 @@ create_blotter_portfolio <- function(p_name, trades, start_date = '2021-02-01') 
         stock(coin, currency = "USD", multiplier = 1)
     getSymbols(coins, from=start_date)
     
-    try(rm(paste("portfolio.", p_name, sep = ""), pos=.blotter), silent = TRUE)
+    try(rm(list=paste("portfolio.", p_name, sep = ""), pos=.blotter))
     initPortf(name = p_name, symbols = coins, initDate = start_date)
     
     trades <- trades %>% 
@@ -55,4 +55,39 @@ create_blotter_portfolio <- function(p_name, trades, start_date = '2021-02-01') 
                                                  TxnPrice = p)
                                           return(T)
                                       }))
+}
+
+
+if (F) {
+    updatePortf('binance')
+    pt <- perTradeStats('binance', coins)
+    pt <- pt %>% select(Symbol, End.Pos, Num.Txns, Max.Notional.Cost, Net.Trading.PL, MAE, Pct.MAE, MFE, Pct.MFE)
+    
+    p <- getPortfolio('binance')
+    t2 <- lapply(coins, function(x) {
+        tail(p$symbols[[x]]$txn, 1)
+    })
+    names(t2) <- coins
+    t3 <- bind_rows(t2, .id = "Symbol")
+    
+    t3 <- t3 %>% select(Symbol, Pos.Avg.Cost)
+    
+    pt %>% 
+        left_join(t3) %>% View
+}
+
+if(F) {
+    th <- readxl::read_xlsx("~/Projects/misc/Trade_History)new.xlsx")
+    th <- th %>% transmute("order_id" = 11000, symbol = Market, price = as.numeric(Price), 
+                  qty = as.numeric(Amount), quote_qty = as.numeric(Total), 
+                  commission = as.numeric(Fee),
+                  commission_asset = `Fee Coin`, time = as.Date(`Date(UTC)`),
+                  is_buyer = Type == "BUY", side = ifelse(is_buyer, yes = 1, no =  -1),
+                  side = side * qty)
+    
+    th <- th %>% 
+        left_join(select(bwallet, coin, yfin_coin), by = c("symbol" = "coin"))
+    
+    trades <- trades %>% 
+        bind_rows(th)
 }
